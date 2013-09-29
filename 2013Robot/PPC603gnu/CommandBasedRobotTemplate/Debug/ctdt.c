d333016c512ab06db52d230853d5571c63ffad73
Ryan Sjostrand <theyoutbesevergeek@gmail.com>
2013-09-28 22:50:35 -0500

    Added inital copy

---
+++ 2013Robot/PPC603gnu/CommandBasedRobotTemplate/Debug/ctdt.c
@@ -0,0 +1,329 @@
+/* ctors and dtors arrays -- to be used by runtime system */
+/*   to call static constructors and destructors          */
+/*                                                        */
+/* NOTE: Use a C compiler to compile this file. If you    */
+/*       are using GNU C++, be sure to use compile this   */
+/*       file using a GNU compiler with the               */
+/*       -fdollars-in-identifiers flag.                   */
+
+
+void _GLOBAL__I__ZN17AutonomousCommandC2Ev();
+
+void _GLOBAL__I__ZN16DeployPanCommandC2Ev();
+
+void _GLOBAL__I__ZN27FrisbeePanSuperVisorCommandC2Ev();
+
+void _GLOBAL__I__ZN23KickerSupervisorCommandC2Ev();
+
+void _GLOBAL__I__ZN20LEDSupervisorCommandC2Ev();
+
+void _GLOBAL__I__ZN21MovePBToBottomCommandC2Ev();
+
+void _GLOBAL__I__ZN18MovePBToTopCommandC2Ev();
+
+void _GLOBAL__I__ZN25PanAtFeederStationCommandC2Ev();
+
+void _GLOBAL__I__ZN25PizzaBoxSupervisorCommandC2Ev();
+
+void _GLOBAL__I__ZN30ShootAndIndexOneFrisbeeCommandC2Ev();
+
+void _GLOBAL__I__ZN33ShooterAndIndexAllFrisbeesCommandC2Ev();
+
+void _GLOBAL__I__ZN25ShooterSupervisiorCommandC2Ev();
+
+void _GLOBAL__I__ZN28ShooterTiltSupervisorCommandC2Ev();
+
+void _GLOBAL__I__ZN14StowPanCommandC2Ev();
+
+void _GLOBAL__I__ZN26TiltShooterTiltDownCommandC2Ev();
+
+void _GLOBAL__I__ZN20TiltShooterUpCommandC2Ev();
+
+void _GLOBAL__I__ZN2OIC2Ev();
+
+void _GLOBAL__I__ZN5Robot7chassisE();
+
+void _GLOBAL__I__ZN8RobotMap21chassisLeftDriveTalonE();
+
+void _GLOBAL__I__ZN7ChassisC2Ev();
+
+void _GLOBAL__I__ZN19FrisbeePanSubsystemC2Ev();
+
+void _GLOBAL__I__ZN15KickerSubsystemC2Ev();
+
+void _GLOBAL__I__ZN12LEDSubsystemC2Ev();
+
+void _GLOBAL__I__ZN19LcdDisplaySubsystemC2Ev();
+
+void _GLOBAL__I__ZN17PizzaBoxSubsystemC2Ev();
+
+void _GLOBAL__I__ZN15SensorSubsystemC2Ev();
+
+void _GLOBAL__I__ZN16ShooterSubsystemC2Ev();
+
+void _GLOBAL__I__ZN20ShooterTiltSubsystemC2Ev();
+
+void _GLOBAL__I__ZN12PrintCommandC2EPKc();
+
+void _GLOBAL__I__ZN9Scheduler9_instanceE();
+
+void _GLOBAL__I__ZN11WaitCommandC2Ed();
+
+void _GLOBAL__I_wpi_error_s_ModuleIndexOutOfRange();
+
+void _GLOBAL__I__ZN10LiveWindow10m_instanceE();
+
+void _GLOBAL__I__ZN17MotorSafetyHelper12m_headHelperE();
+
+void _GLOBAL__I__ZN8Notifier21kTimerInterruptNumberE();
+
+void _GLOBAL__I__ZN8Resource12m_createLockE();
+
+void _GLOBAL__I__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__I__ZN14SmartDashboard7m_tableE();
+
+void _GLOBAL__I__ZN12SolenoidBase11m_semaphoreE();
+
+void _GLOBAL__I__ZN10Ultrasonic9kPingTimeE();
+
+void _GLOBAL__I_AxisCamera_debugFlag();
+
+void _GLOBAL__I__ZN12NetworkTable19PATH_SEPARATOR_CHARE();
+
+void _GLOBAL__I__ZN16NetworkTableMode6ServerE();
+
+void _GLOBAL__I__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager();
+
+void _GLOBAL__I_OutgoingEntryReceiver_NULL();
+
+void _GLOBAL__I__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem();
+
+void _GLOBAL__I__ZN21ClientConnectionState24DISCONNECTED_FROM_SERVERE();
+
+void _GLOBAL__I__ZN12DataIOStreamC2EP8IOStream();
+
+void _GLOBAL__I__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager();
+
+void _GLOBAL__I__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE();
+
+void _GLOBAL__I__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE();
+
+void _GLOBAL__I__ZN17DefaultEntryTypes7BOOLEANE();
+
+void _GLOBAL__I__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE();
+
+void _GLOBAL__I__ZN11StringArray19STRING_ARRAY_RAW_IDE();
+
+extern void (*_ctors[])();
+void (*_ctors[])() =
+    {
+    _GLOBAL__I__ZN17AutonomousCommandC2Ev,
+    _GLOBAL__I__ZN16DeployPanCommandC2Ev,
+    _GLOBAL__I__ZN27FrisbeePanSuperVisorCommandC2Ev,
+    _GLOBAL__I__ZN23KickerSupervisorCommandC2Ev,
+    _GLOBAL__I__ZN20LEDSupervisorCommandC2Ev,
+    _GLOBAL__I__ZN21MovePBToBottomCommandC2Ev,
+    _GLOBAL__I__ZN18MovePBToTopCommandC2Ev,
+    _GLOBAL__I__ZN25PanAtFeederStationCommandC2Ev,
+    _GLOBAL__I__ZN25PizzaBoxSupervisorCommandC2Ev,
+    _GLOBAL__I__ZN30ShootAndIndexOneFrisbeeCommandC2Ev,
+    _GLOBAL__I__ZN33ShooterAndIndexAllFrisbeesCommandC2Ev,
+    _GLOBAL__I__ZN25ShooterSupervisiorCommandC2Ev,
+    _GLOBAL__I__ZN28ShooterTiltSupervisorCommandC2Ev,
+    _GLOBAL__I__ZN14StowPanCommandC2Ev,
+    _GLOBAL__I__ZN26TiltShooterTiltDownCommandC2Ev,
+    _GLOBAL__I__ZN20TiltShooterUpCommandC2Ev,
+    _GLOBAL__I__ZN2OIC2Ev,
+    _GLOBAL__I__ZN5Robot7chassisE,
+    _GLOBAL__I__ZN8RobotMap21chassisLeftDriveTalonE,
+    _GLOBAL__I__ZN7ChassisC2Ev,
+    _GLOBAL__I__ZN19FrisbeePanSubsystemC2Ev,
+    _GLOBAL__I__ZN15KickerSubsystemC2Ev,
+    _GLOBAL__I__ZN12LEDSubsystemC2Ev,
+    _GLOBAL__I__ZN19LcdDisplaySubsystemC2Ev,
+    _GLOBAL__I__ZN17PizzaBoxSubsystemC2Ev,
+    _GLOBAL__I__ZN15SensorSubsystemC2Ev,
+    _GLOBAL__I__ZN16ShooterSubsystemC2Ev,
+    _GLOBAL__I__ZN20ShooterTiltSubsystemC2Ev,
+    _GLOBAL__I__ZN12PrintCommandC2EPKc,
+    _GLOBAL__I__ZN9Scheduler9_instanceE,
+    _GLOBAL__I__ZN11WaitCommandC2Ed,
+    _GLOBAL__I_wpi_error_s_ModuleIndexOutOfRange,
+    _GLOBAL__I__ZN10LiveWindow10m_instanceE,
+    _GLOBAL__I__ZN17MotorSafetyHelper12m_headHelperE,
+    _GLOBAL__I__ZN8Notifier21kTimerInterruptNumberE,
+    _GLOBAL__I__ZN8Resource12m_createLockE,
+    _GLOBAL__I__ZN9RobotBase10m_instanceE,
+    _GLOBAL__I__ZN14SmartDashboard7m_tableE,
+    _GLOBAL__I__ZN12SolenoidBase11m_semaphoreE,
+    _GLOBAL__I__ZN10Ultrasonic9kPingTimeE,
+    _GLOBAL__I_AxisCamera_debugFlag,
+    _GLOBAL__I__ZN12NetworkTable19PATH_SEPARATOR_CHARE,
+    _GLOBAL__I__ZN16NetworkTableMode6ServerE,
+    _GLOBAL__I__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager,
+    _GLOBAL__I_OutgoingEntryReceiver_NULL,
+    _GLOBAL__I__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem,
+    _GLOBAL__I__ZN21ClientConnectionState24DISCONNECTED_FROM_SERVERE,
+    _GLOBAL__I__ZN12DataIOStreamC2EP8IOStream,
+    _GLOBAL__I__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager,
+    _GLOBAL__I__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE,
+    _GLOBAL__I__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE,
+    _GLOBAL__I__ZN17DefaultEntryTypes7BOOLEANE,
+    _GLOBAL__I__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE,
+    _GLOBAL__I__ZN11StringArray19STRING_ARRAY_RAW_IDE,
+    0
+    };
+
+void _GLOBAL__D__ZN17AutonomousCommandC2Ev();
+
+void _GLOBAL__D__ZN16DeployPanCommandC2Ev();
+
+void _GLOBAL__D__ZN27FrisbeePanSuperVisorCommandC2Ev();
+
+void _GLOBAL__D__ZN23KickerSupervisorCommandC2Ev();
+
+void _GLOBAL__D__ZN20LEDSupervisorCommandC2Ev();
+
+void _GLOBAL__D__ZN21MovePBToBottomCommandC2Ev();
+
+void _GLOBAL__D__ZN18MovePBToTopCommandC2Ev();
+
+void _GLOBAL__D__ZN25PanAtFeederStationCommandC2Ev();
+
+void _GLOBAL__D__ZN25PizzaBoxSupervisorCommandC2Ev();
+
+void _GLOBAL__D__ZN30ShootAndIndexOneFrisbeeCommandC2Ev();
+
+void _GLOBAL__D__ZN33ShooterAndIndexAllFrisbeesCommandC2Ev();
+
+void _GLOBAL__D__ZN25ShooterSupervisiorCommandC2Ev();
+
+void _GLOBAL__D__ZN28ShooterTiltSupervisorCommandC2Ev();
+
+void _GLOBAL__D__ZN14StowPanCommandC2Ev();
+
+void _GLOBAL__D__ZN26TiltShooterTiltDownCommandC2Ev();
+
+void _GLOBAL__D__ZN20TiltShooterUpCommandC2Ev();
+
+void _GLOBAL__D__ZN2OIC2Ev();
+
+void _GLOBAL__D__ZN5Robot7chassisE();
+
+void _GLOBAL__D__ZN8RobotMap21chassisLeftDriveTalonE();
+
+void _GLOBAL__D__ZN7ChassisC2Ev();
+
+void _GLOBAL__D__ZN19FrisbeePanSubsystemC2Ev();
+
+void _GLOBAL__D__ZN15KickerSubsystemC2Ev();
+
+void _GLOBAL__D__ZN12LEDSubsystemC2Ev();
+
+void _GLOBAL__D__ZN19LcdDisplaySubsystemC2Ev();
+
+void _GLOBAL__D__ZN17PizzaBoxSubsystemC2Ev();
+
+void _GLOBAL__D__ZN15SensorSubsystemC2Ev();
+
+void _GLOBAL__D__ZN16ShooterSubsystemC2Ev();
+
+void _GLOBAL__D__ZN20ShooterTiltSubsystemC2Ev();
+
+void _GLOBAL__D__ZN9Scheduler9_instanceE();
+
+void _GLOBAL__D_wpi_error_s_ModuleIndexOutOfRange();
+
+void _GLOBAL__D__ZN17MotorSafetyHelper12m_headHelperE();
+
+void _GLOBAL__D__ZN8Notifier21kTimerInterruptNumberE();
+
+void _GLOBAL__D__ZN8Resource12m_createLockE();
+
+void _GLOBAL__D__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__D__ZN14SmartDashboard7m_tableE();
+
+void _GLOBAL__D__ZN12SolenoidBase11m_semaphoreE();
+
+void _GLOBAL__D__ZN10Ultrasonic9kPingTimeE();
+
+void _GLOBAL__D_AxisCamera_debugFlag();
+
+void _GLOBAL__D__ZN12NetworkTable19PATH_SEPARATOR_CHARE();
+
+void _GLOBAL__D__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager();
+
+void _GLOBAL__D_OutgoingEntryReceiver_NULL();
+
+void _GLOBAL__D__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem();
+
+void _GLOBAL__D__ZN12DataIOStreamC2EP8IOStream();
+
+void _GLOBAL__D__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager();
+
+void _GLOBAL__D__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE();
+
+void _GLOBAL__D__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE();
+
+void _GLOBAL__D__ZN17DefaultEntryTypes7BOOLEANE();
+
+void _GLOBAL__D__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE();
+
+void _GLOBAL__D__ZN11StringArray19STRING_ARRAY_RAW_IDE();
+
+extern void (*_dtors[])();
+void (*_dtors[])() =
+    {
+    _GLOBAL__D__ZN17AutonomousCommandC2Ev,
+    _GLOBAL__D__ZN16DeployPanCommandC2Ev,
+    _GLOBAL__D__ZN27FrisbeePanSuperVisorCommandC2Ev,
+    _GLOBAL__D__ZN23KickerSupervisorCommandC2Ev,
+    _GLOBAL__D__ZN20LEDSupervisorCommandC2Ev,
+    _GLOBAL__D__ZN21MovePBToBottomCommandC2Ev,
+    _GLOBAL__D__ZN18MovePBToTopCommandC2Ev,
+    _GLOBAL__D__ZN25PanAtFeederStationCommandC2Ev,
+    _GLOBAL__D__ZN25PizzaBoxSupervisorCommandC2Ev,
+    _GLOBAL__D__ZN30ShootAndIndexOneFrisbeeCommandC2Ev,
+    _GLOBAL__D__ZN33ShooterAndIndexAllFrisbeesCommandC2Ev,
+    _GLOBAL__D__ZN25ShooterSupervisiorCommandC2Ev,
+    _GLOBAL__D__ZN28ShooterTiltSupervisorCommandC2Ev,
+    _GLOBAL__D__ZN14StowPanCommandC2Ev,
+    _GLOBAL__D__ZN26TiltShooterTiltDownCommandC2Ev,
+    _GLOBAL__D__ZN20TiltShooterUpCommandC2Ev,
+    _GLOBAL__D__ZN2OIC2Ev,
+    _GLOBAL__D__ZN5Robot7chassisE,
+    _GLOBAL__D__ZN8RobotMap21chassisLeftDriveTalonE,
+    _GLOBAL__D__ZN7ChassisC2Ev,
+    _GLOBAL__D__ZN19FrisbeePanSubsystemC2Ev,
+    _GLOBAL__D__ZN15KickerSubsystemC2Ev,
+    _GLOBAL__D__ZN12LEDSubsystemC2Ev,
+    _GLOBAL__D__ZN19LcdDisplaySubsystemC2Ev,
+    _GLOBAL__D__ZN17PizzaBoxSubsystemC2Ev,
+    _GLOBAL__D__ZN15SensorSubsystemC2Ev,
+    _GLOBAL__D__ZN16ShooterSubsystemC2Ev,
+    _GLOBAL__D__ZN20ShooterTiltSubsystemC2Ev,
+    _GLOBAL__D__ZN9Scheduler9_instanceE,
+    _GLOBAL__D_wpi_error_s_ModuleIndexOutOfRange,
+    _GLOBAL__D__ZN17MotorSafetyHelper12m_headHelperE,
+    _GLOBAL__D__ZN8Notifier21kTimerInterruptNumberE,
+    _GLOBAL__D__ZN8Resource12m_createLockE,
+    _GLOBAL__D__ZN9RobotBase10m_instanceE,
+    _GLOBAL__D__ZN14SmartDashboard7m_tableE,
+    _GLOBAL__D__ZN12SolenoidBase11m_semaphoreE,
+    _GLOBAL__D__ZN10Ultrasonic9kPingTimeE,
+    _GLOBAL__D_AxisCamera_debugFlag,
+    _GLOBAL__D__ZN12NetworkTable19PATH_SEPARATOR_CHARE,
+    _GLOBAL__D__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager,
+    _GLOBAL__D_OutgoingEntryReceiver_NULL,
+    _GLOBAL__D__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem,
+    _GLOBAL__D__ZN12DataIOStreamC2EP8IOStream,
+    _GLOBAL__D__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager,
+    _GLOBAL__D__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE,
+    _GLOBAL__D__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE,
+    _GLOBAL__D__ZN17DefaultEntryTypes7BOOLEANE,
+    _GLOBAL__D__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE,
+    _GLOBAL__D__ZN11StringArray19STRING_ARRAY_RAW_IDE,
+    0
+    };
